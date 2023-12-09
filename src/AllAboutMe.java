c2ce75bfe0c3f1a50ba5c52fb26eaa51bc59d3bd
HodaJalayeri <jalayeri.hoda@gmail.com>
2023-12-09 10:42:24 -0600

    initial comment

---
+++ src/AllAboutMe.java
@@ -0,0 +1,18 @@
+public class AllAboutMe {
+    public static void main (String[] args) {
+        String name = "Hoda";
+        String favoriteFood = "Pizza";
+        String numberOfPet = "0";
+        String placeOfLive = "House";
+        String abilityToWristle = "Yes";
+        System.out.println("My Name is" + " " + name + ".");
+        System.out.println("My favorite food is" + " "+ favoriteFood);
+        System.out.println("I have" + " "+ numberOfPet + " "+"Pet.");
+        System.out.println("I live in a" + " "+ placeOfLive + ", and I love it here.");
+        System.out.println(abilityToWristle + "," + "It is true I know how to whistle.");
+    }
+
+
+    }
+
+
